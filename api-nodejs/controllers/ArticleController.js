f26dfa3c35178ca2115352524f47b28b4f245fbf
Wilson Aguilar <waguilars@est.ups.edu.ec>
2020-04-02 19:37:53 -0500

    metodos de get update delete listo

--- api-nodejs/controllers/ArticleController.js
+++ api-nodejs/controllers/ArticleController.js
@@ -55,4 +55,141 @@ articleController.save = async(req, res) => {
     }
 }
 
+articleController.getArticles = async(req, res) => {
+    const query = Article.find({});
+    const items = +req.params.last;
+    if (items) {
+        query.limit(items)
+    }
+
+    try {
+        //find
+        const articles = await query.sort('-_id').exec();
+
+        return res.status(200).json({
+            status: 'ok',
+            articles
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            status: 'error',
+            error
+        });
+    }
+}
+
+articleController.getArticle = async(req, res) => {
+    // recoger id de url
+    const id = req.params.id;
+
+    // comprobar si existe 
+    if (!id) {
+        return res.status(404).json({
+            status: 'error',
+            message: 'No existe el articulo.'
+        });
+    }
+
+    try {
+        // buscar el articulo
+        const article = await Article.findById(id).exec();
+        if (!article) {
+            return res.status(404).json({
+                status: 'error',
+                message: 'No existe el articulo.'
+            });
+        }
+
+        // devoler res
+        return res.status(200).json({
+            status: 'ok',
+            article: article
+        });
+    } catch (error) {
+        return res.status(500).json({
+            status: 'error',
+            message: 'Error al procesar la peticion.'
+        });
+
+    }
+
+}
+
+articleController.update = async(req, res) => {
+    //recoger id de articulo
+    const id = req.params.id
+
+    // recoger data
+    const params = req.body
+
+    // validata data
+    try {
+        const vTitle = validator.isEmpty(params.title);
+        const vContent = validator.isEmpty(params.content);
+
+        if (vTitle || vContent) {
+            return res.status(400).json({
+                status: 'error',
+                message: 'Por favor complete los campos.'
+            })
+        }
+        // find and update
+        const article = await Article.findOneAndUpdate({ _id: id }, params, { new: true }).exec();
+
+        if (!article) {
+            return res.status(404).json({
+                status: 'error',
+                message: 'El articulo que desea actualizar no existe.'
+            });
+        }
+        //devolver res
+        return res.status(200).json({
+            status: 'ok',
+            article
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            status: 'error',
+            message: 'Error al procesar la peticion.'
+        });
+    }
+
+
+
+
+
+
+}
+
+articleController.delete = async(req, res) => {
+    try {
+        // recoger id
+        const id = req.params.id;
+
+        // Eliminar
+        const article = await Article.findByIdAndDelete({ _id: id }).exec();
+        if (!article) {
+            return res.status(404).json({
+                status: 'error',
+                message: 'El articulo que desea eliminar no existe.'
+            })
+        }
+
+        // devolver res
+        return res.status(200).json({
+            status: 'ok',
+            article
+        });
+    } catch (error) {
+        return res.status(500).json({
+            status: 'error',
+            message: 'Error al procesar la solicutud.'
+        });
+
+    }
+
+}
+
 module.exports = articleController;
--- api-nodejs/routes/ArticleRoutes.js
+++ api-nodejs/routes/ArticleRoutes.js
@@ -5,6 +5,10 @@ const articleController = require('../controllers/ArticleController');
 const router = express.Router();
 
 
-router.post('/save', articleController.save)
+router.post('/save', articleController.save);
+router.get('/articles/:last?', articleController.getArticles);
+router.get('/article/:id', articleController.getArticle);
+router.put('/article/:id', articleController.update);
+router.delete('/article/:id', articleController.delete);
 
 module.exports = router;
