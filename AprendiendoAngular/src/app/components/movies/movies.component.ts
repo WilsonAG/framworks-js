22af3f786b5eb27d7102f25bd40d87a260006187
Wilson Aguilar <waguilars@est.ups.edu.ec>
2020-04-04 21:09:35 -0500

    Directivas

--- AprendiendoAngular/src/app/components/movies/movies.component.ts
+++ AprendiendoAngular/src/app/components/movies/movies.component.ts
@@ -7,10 +7,18 @@ import { Component, OnInit, DoCheck, OnDestroy } from '@angular/core';
 })
 export class MoviesComponent implements OnInit, DoCheck, OnDestroy {
     public titulo: string;
+    public movies: Array<Imovie>;
 
     constructor() {
         this.titulo = 'Componente de peliculas';
-        console.log('Contructor lanzado');
+        this.movies = [
+            { year: 2019, title: 'Spiderman', image: 'https://pbs.twimg.com/media/D9M5NIXXkAAtAk_?format=jpg&name=small' },
+            { year: 2012, title: 'Avengers', image: 'https://cnet2.cbsistatic.com/img/VcjARNnYtd1qwbOCueNf3bnOlhc=/1092x0/2019/03/14/70b49c1d-0d3b-4b75-9225-b898b83cdc9a/avengers-endgame-poster-og-social-crop.jpg' },
+            { year: 2019, title: 'Batman v Superman', image: 'https://es.web.img2.acsta.net/pictures/16/03/04/15/47/480948.jpg' },
+            { year: 2015, title: 'Batman v Superman', image: 'https://es.web.img2.acsta.net/pictures/16/03/04/15/47/480948.jpg' },
+        ];
+
+        console.log(this.movies);
     }
     ngOnDestroy(): void {
         console.log('El componente se va a destruir');
@@ -27,4 +35,10 @@ export class MoviesComponent implements OnInit, DoCheck, OnDestroy {
         this.titulo = 'El titulo cambio';
     }
 
+}
+
+interface Imovie {
+    year: number;
+    title: string;
+    image: string;
 }
