9142d3ff83382a8b33204e32c6197b822acfc9da
Wilson Aguilar <waguilars@est.ups.edu.ec>
2020-04-25 20:46:04 -0500

    vue - peticiones http

---
+++ AprendiendoVueJs/src/components/Articles.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="list-articles">
+    <article class="article-item" id="article-item" v-for="article in articles" :key="article._id">
+      <div class="image-wrap">
+        <img v-if="article.image" :src="uri+'image/'+article.image" :alt="article.title" />
+        <img v-if="!article.image" src="../assets/logo.png" :alt="article.title" />
+      </div>
+      <h2>{{article.title}}</h2>
+      <span class="date">{{article.date}}</span>
+      <a href="#">Leer mas</a>
+      <div class="clearfix"></div>
+    </article>
+  </div>
+</template>
+
+<script>
+import Global from "../global";
+
+export default {
+  name: "Articles",
+  props: ["articles"],
+  data() {
+    return {
+      uri: Global.uri
+    };
+  }
+};
+</script>
+
+<style>
+</style>
--- AprendiendoVueJs/src/components/Blog.vue
+++ AprendiendoVueJs/src/components/Blog.vue
@@ -4,6 +4,8 @@
     <div class="center">
       <section class="content">
         <h2 class="subheader">Blog</h2>
+        <div id="articles" v-if="articles.length > 0"></div>
+        <Articles :articles="articles" />
       </section>
       <Sidebar />
     </div>
@@ -14,12 +16,34 @@
 <script>
 import Sidebar from "./Sidebar.vue";
 import Slider from "./Slider.vue";
+import Articles from "./Articles.vue";
+import Axios from "axios";
+import Global from "../global";
 
 export default {
   name: "Blog",
   components: {
     Sidebar,
-    Slider
+    Slider,
+    Articles
+  },
+  mounted() {
+    this.getArticles();
+  },
+  methods: {
+    getArticles() {
+      Axios.get(Global.uri + "articles").then(res => {
+        if (res.data.status === "ok") {
+          this.articles = res.data.articles;
+        }
+        console.log(this.articles);
+      });
+    }
+  },
+  data() {
+    return {
+      articles: []
+    };
   }
 };
 </script>
